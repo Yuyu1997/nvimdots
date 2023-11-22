3c84862a3fea18281d77e628b6690ce10c38aae8
Yuyu <shixinyu@trunk.tech>
2023-11-22 17:24:57 +0800

    refactor(style, clangd, clang-format)

--- lua/modules/configs/completion/formatters/clang_format.lua
+++ lua/modules/configs/completion/formatters/clang_format.lua
@@ -1,1 +1,5 @@
-return { "-style={BasedOnStyle: LLVM, IndentWidth: 4}" }
+--return { "-style={BasedOnStyle: LLVM, IndentWidth: 4}" }
+--return { "--style=/home/trunk/.clang-format" }
+return {
+	"-style={BasedOnStyle: Google, Language: Cpp, Cpp11BracedListStyle: true, Standard: Cpp11, CommentPragmas: '^ NOLINT', AllowShortIfStatementsOnASingleLine: false, AllowShortLoopsOnASingleLine: false, AllowShortCaseLabelsOnASingleLine: false, AllowShortBlocksOnASingleLine: false, AlignTrailingComments: true, DerivePointerAlignment: false, PointerAlignment: Left, MaxEmptyLinesToKeep: 1}",
+}
